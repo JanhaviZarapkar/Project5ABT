a1df7f7c1a3bdbd3c126db5dfe3966875469ab21
Shashank Gupta <35930667+ShshnkGpta@users.noreply.github.com>
2021-09-22 17:50:26 +0530

    Update LoanSchedule.js

--- home_loan_ui/src/Home/LoanSchedule.js
+++ home_loan_ui/src/Home/LoanSchedule.js
@@ -1,7 +1,57 @@
 import React from "react";
 
+import React from "react";
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
+function createData(date, emi, principal, interest, oustanding, status) {
+  return { date, emi, principal, interest, oustanding, status };
+}
+
+const rows = [
+  createData('Sep 2021', 10000, 7500, 2500, 42500, 'Pending'),
+  createData('Oct 2021', 10000, 8000, 2000, 34500, 'Pending'),
+];
+
 const LoanSchedule = () => {
-  return <div>Loan Schedule</div>;
+  return(
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Date</TableCell>
+            <TableCell align="right">EMI</TableCell>
+            <TableCell align="right">Principal</TableCell>
+            <TableCell align="right">Interest</TableCell>
+            <TableCell align="right">Oustanding</TableCell>
+            <TableCell align="right">Payment Status</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow
+              key={row.date}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {row.date}
+              </TableCell>
+              <TableCell align="right">{row.emi}</TableCell>
+              <TableCell align="right">{row.principal}</TableCell>
+              <TableCell align="right">{row.interest}</TableCell>
+              <TableCell align="right">{row.oustanding}</TableCell>
+              <TableCell align="right">{row.status}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
 };
 
 export default LoanSchedule;
