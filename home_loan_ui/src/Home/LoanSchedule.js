64fc1e2ccf8437375d62b50ab34c90387788cc22
Naman Dutta <37544286+duttanaman1@users.noreply.github.com>
2021-09-23 09:50:41 +0530

    Update LoanSchedule.js

--- home_loan_ui/src/Home/LoanSchedule.js
+++ home_loan_ui/src/Home/LoanSchedule.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import { CSVLink } from "react-csv";
 import axios from "axios";
 import Container from "@mui/material/Container";
 import Header from "./Header";
@@ -9,22 +10,46 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
+import Button from "@mui/material/Button";
 
 function createData(date, emi, principal, interest, oustanding, status) {
   return { date, emi, principal, interest, oustanding, status };
 }
-
+const payment = (id) => {
+  axios.put().then((res) => {
+    var response = res.data;
+    var temp = rows.filter((data) => {
+      data.id != id;
+    });
+    setData(temp);
+  });
+};
+const [datas, setData] = useState();
 const rows = [
   createData("Sep 2021", 10000, 7500, 2500, 42500, "Pending"),
   createData("Oct 2021", 10000, 8000, 2000, 34500, "Pending"),
 ];
+const headers = [
+  { label: "Date", key: "firstName" },
+  { label: "EMI", key: "lastName" },
+  { label: "Principal", key: "email" },
+  { label: "Interest", key: "age" },
+  { label: "outstanding", key: "age" },
+  { label: "status", key: "age" },
+];
 
+const csvReport = {
+  data: datas,
+  headers: headers,
+  filename: "report",
+};
 const LoanSchedule = () => {
   return (
     <div>
       <Header />
       <Container style={{ marginTop: "5%" }}>
         <h1>Loan Schedule</h1>
+        <csvlink {...csvreport}>Download</csvlink>
         <TableContainer component={Paper}>
           <Table sx={{ minWidth: 650 }} aria-label="simple table">
             <TableHead>
@@ -35,6 +60,7 @@ const LoanSchedule = () => {
                 <TableCell align="right">Interest</TableCell>
                 <TableCell align="right">Oustanding</TableCell>
                 <TableCell align="right">Payment Status</TableCell>
+                <TableCell align="right">Payment</TableCell>
               </TableRow>
             </TableHead>
             <TableBody>
@@ -51,6 +77,18 @@ const LoanSchedule = () => {
                   <TableCell align="right">{row.interest}</TableCell>
                   <TableCell align="right">{row.oustanding}</TableCell>
                   <TableCell align="right">{row.status}</TableCell>
+                  <TableCell align="right">
+                    <Button
+                      variant="contained"
+                      size="large"
+                      style={{ width: "50%" }}
+                      onClick={() => {
+                        payment(row.date);
+                      }}
+                    >
+                      Payment
+                    </Button>
+                  </TableCell>
                 </TableRow>
               ))}
             </TableBody>
