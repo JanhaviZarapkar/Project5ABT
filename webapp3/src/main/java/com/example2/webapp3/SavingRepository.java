920beeaa2e85897239bd653a70585359eed772a3
harsh <harshlohana38@gmail.com>
2021-09-23 15:55:33 +0530

    Final

--- webapp3/src/main/java/com/example2/webapp3/SavingRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example2.webapp3;
-import java.util.Optional;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SavingRepository extends JpaRepository<SavingAccount,Integer> {
-
-    
-}
